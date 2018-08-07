fd14c7f87733318e78a3e6260716a6432b8c3a47
Mark SUKHAREV <msukhare@e3r13p14.42.fr>
2018-08-07 10:57:29 +0200

    ajout de la backprop

--- train_multi_percep.py
+++ train_multi_percep.py
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    train_multi_percep.py                              :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: msukhare <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/08/06 16:36:59 by msukhare          #+#    #+#              #
+#    Updated: 2018/08/07 10:57:12 by msukhare         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 import pandas as pd
 import numpy as np
 import sys
@@ -36,22 +48,18 @@ def scaling_feat(data):
 def sigmoid(z):
     return (1 / (1 + np.exp(-z)))
 
-def forward_prop(X, thetas1, thetas2, thetas3, layer):
+def forward_prop(X, thetas1, thetas2, thetas3):
     l2 = sigmoid(np.dot(thetas1, X))
-    if (layer == 2):
-        return (l2)
     l3 = sigmoid(np.dot(thetas2, l2))
-    if (layer == 3):
-        return (l3)
     return (sigmoid(np.dot(thetas3, l3)))
 
 def get_summ(X, Y, thetas1, thetas2, thetas3, m):
     summ = 0
     for i in range(int(m)):
         if (Y[i] == 1):
-            summ += np.log(forward_prop(X[i], thetas1, thetas2, thetas3, 4))
+            summ += np.log(forward_prop(X[i], thetas1, thetas2, thetas3))
         else:
-            summ += np.log(1 - forward_prop(X[i], thetas1, thetas2, thetas3, 4))
+            summ += np.log(1 - forward_prop(X[i], thetas1, thetas2, thetas3))
     return (summ)
 
 def cost_fct(X, Y, thetas1, thetas2, thetas3):
@@ -59,16 +67,31 @@ def cost_fct(X, Y, thetas1, thetas2, thetas3):
     return (-(1 / m) * get_summ(X, Y, thetas1, thetas2, thetas3, m))
 
 def back_prop(X, Y, thetas1, thetas2, thetas3):
-    for i in range(int(X.shape[0])):
-        l4 = forward_prop(X[i], thetas1, thetas2, thetas3, 4)
-        error4 = l4 - Y[i]
-        l3 = forward_prop(X[i], thetas1, thetas2, thetas3, 3)
-        error3 = (thetas3.transpose() * error4).dot(l3.dot(1 - l3))
-        l2 = forward_prop(X[i], thetas1, thetas2, thetas3, 2)
-        error2 = (thetas2.transpose() * error3).dot(l2.dot(1 - l2))
-        tmp = 0 + error2.dot(X[i].transpose())
-        print(tmp, tmp.shape[0])
-        sys.exit()
+    m = X.shape[0]
+    capital_greek1 = np.zeros((36, 31), dtype=float)
+    capital_greek2 = np.zeros((36, 36), dtype=float)
+    capital_greek3 = np.zeros((1, 36), dtype=float)
+    for i in range(int(m)):
+        l1 = X[i]
+        l2 = sigmoid(np.dot(thetas1, l1))
+        l3 = sigmoid(np.dot(thetas2, l2))
+        l4 = sigmoid(np.dot(thetas3, l3))
+        lower_case_delta4 = l4 - Y[i]
+        lower_case_delta3 = (thetas3.transpose().dot(lower_case_delta4)).dot(l3) * (1 - l3)
+        lower_case_delta2 = (thetas2.transpose().dot(lower_case_delta3)).dot(l2) * (1 - l2)
+        lower_case_delta2 = np.reshape(lower_case_delta2, (36, 1))
+        lower_case_delta3 = np.reshape(lower_case_delta3, (36, 1))
+        lower_case_delta4 = np.reshape(lower_case_delta4, (1, 1))
+        l1 = np.reshape(l1, (1, 31))
+        capital_greek1 += lower_case_delta2.dot(l1)
+        l2 = np.reshape(l2, (1, 36))
+        capital_greek2 += lower_case_delta3.dot(l2)
+        l3 = np.reshape(l3, (1, 36))
+        capital_greek3 += lower_case_delta4.dot(l3)
+    capital_greek1 = 1 / m * capital_greek1
+    capital_greek2 = 1 / m * capital_greek2
+    capital_greek3 = 1 / m * capital_greek3
+    return (capital_greek1, capital_greek2, capital_greek3)
 
 def main():
     check_argv()
@@ -79,7 +102,7 @@ def main():
     thetas2 = (np.random.rand(36, 36) * (2 * epsilon) - epsilon)
     thetas3 = (np.random.rand(1, 36) * (2 * epsilon) - epsilon)
     cost_fct(X, Y, thetas1, thetas2, thetas3)
-    back_prop(X, Y, thetas1, thetas2, thetas3)
+    cap1, cap2, cap3 = back_prop(X, Y, thetas1, thetas2, thetas3)
 
 if (__name__ == "__main__"):
     main()
